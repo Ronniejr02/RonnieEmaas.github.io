a45066624524794edc6244347f1c3618b0b4c6a0
RonnieEmaas <143926495+Ronniejr02@users.noreply.github.com>
2024-09-07 10:38:37 +0800

    Create grp1_Emaas_ex1.py

---
+++ grp1_Emaas_ex1.py
@@ -0,0 +1,43 @@
+# Filename: grp1_Emaas_ex1.py
+
+# Comments
+# a. Create a simple print script
+# b. Add comment "My first simple script"
+
+# My first simple script
+print("Hello, I'm Emaas, Ronnie Jr G! BSCS 3F")
+
+# Variables
+# a. Create two variables and assign number and name to it.
+age = 21
+student_name = "Ronnie G. Emaas Jr "
+
+# b. Then print both values
+print("Age:", age)
+print("Student Name:", student_name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+Alvinson = "Alvinson"
+charles = "Charles"
+john_carlo = "John Carlo"  # Changed to use underscores
+rodrigo = "Rodrigo"
+
+# Print the values of the group member variables
+print("Group Member First Names:")
+print(Alvinson)
+print(charles)
+print(john_carlo)
+print(rodrigo)
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+height, weight, shoe_size = 5.7, 59, 40
+print("Height:", height)
+print("Weight:", weight)
+print("Shoe Size:", shoe_size)
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+status1 = status2 = status3 = "Active"
+print("Account Statuses:", status1, status2, status3)
